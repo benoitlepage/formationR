51f90eb138623f5ad54df20624cd6e32193f8b45
Benoit Lepage <benoit.lepage@univ-tlse3.fr>
2025-09-22 15:36:13 +0200

    preparation tidyverse

--- data/analyse 1 tidyverse.R
+++ data/analyse 1 tidyverse.R
@@ -260,6 +260,58 @@ write_csv2(df_1, "data/df_1_new.csv")
 
 
 # 5) Analyses univariées ----
+## 5.0) création de 2 variables de type "factor" à partir des variables sex et trait
+## avec le package forcats
+## https://forcats.tidyverse.org/
+
+### on peut utiliser directement la fonction factor de Rbase
+# df_1$sexL <- factor(df_1$sex,
+#                     labels = meta_df_1$labs[meta_df_1$var == "sex"])
+# df_1$traitL <- factor(df_1$trait,
+#                       labels = meta_df_1$labs[meta_df_1$var == "trait"])
+
+### syntaxe dyplr avec factor au sein de la fonction mutate()
+### (c'est la même fonction factor de Rbase)
+df_1 <- df_1 %>% 
+  mutate(sexL = factor(sex, 
+                       labels = meta_df_1$labs[meta_df_1$var == "sex"]),
+         traitL = factor(trait))
+df_1 # on voit que les deux variables sexL et traitL ont été ajoutées à df_1
+
+### Le package forcats du tidyverse apporte de nombreuses fonctions 
+### pour manipuler les variables de type "factor". Toutes ces fonctions
+### commences par "ftc" (pour "factor").
+### voir le cheatsheet https://raw.githubusercontent.com/rstudio/cheatsheets/main/factors.pdf
+
+
+### Par exemple : 
+
+### les labels n'ont pas été utilisés pour la variable traitL, 
+### on observe donc les codes 1, 2, 3 à la place
+### on peut recoder ces labels à la main avec la fonction fct_recode()
+##  pour se rappeler du codage, on peut aller rechercher dans la base de 
+## métadonnées
+meta_df_1 %>% 
+  filter(var == "trait")
+
+df_1$traitL <- fct_recode(df_1$traitL, 
+                          Placebo = "1", # nouveau label = "ancien label"
+                          "Traitement A" = "2", 
+                          "Traitement B" = "3")
+# s'il y a des caractètres spéciaux (comme des espaces, le nouveau nom doit être
+# entre guillements)
+
+## note que la notation avec le pipe ci-dessous 
+df_1 %>% fct_recode(traitL, 
+                    Placebo = "1", # nouveau label = "ancien label"
+                    "Traitement A" = "2", 
+                    "Traitement B" = "3") 
+## ne fonctionne pas car le premier argument de fct_recode doit être un facteur. 
+## Cette écriture avec le pipe extrait la colonne au format "tibble" 
+## (pas au format "factor") 
+
+
+
 ## 5.1) fonction summarise() ----
 ?summarize()
 # permet de décrire différents paramètres avec les fonctions suivantes à indiquer 
@@ -268,7 +320,7 @@ write_csv2(df_1, "data/df_1_new.csv")
 #  - min, max, médiane et intervalles interquartiles : min(), max(), media() IQR(), quantile()
 #  - compte avec n() et n_distinct()
 
-## 5.2) variables quantitatives
+## 5.2) variables quantitatives ----
 # on peut décrire pour une variable quantitative, 
 # le nombre de valeurs non-manquantes, la moyenne, l'écart type, 
 # le min, le max, l'intervalle interquartile et la médiane
@@ -283,7 +335,7 @@ df_1 %>% summarise(n = sum(!is.na(imc)), # nombre de non-manquants
                    p75 = quantile(imc, probs = 0.75),
                    max = max(imc))
 
-df_1 %>% summarise(mean = mean(imc, pas))
+# df_1 %>% summarise(mean = mean(imc, pas)) # ça ne fonctionne pas
 
 ## pour faire un descriptif de plusieurs variables quantitatif, on peut utiliser 
 ## des fonctions connexes de summurize : summarise_at(), summarise_all(), 
@@ -295,16 +347,374 @@ df_1 %>% summarise_at(c("imc", "pas"),
 # il crée une nouvelle variable à chaque pour la présenter en tibble.
 # pas très pratique pour décrire plus de paramètre sur un grand ensemble de variables
 
-# proposition : faire une boucle, puis bind_rows() ? 
-for(i in c(3,5)) {
-  select(df_1[i]) %>% 
-    summarise(#n = sum(!is.na()), # nombre de non-manquants
-              mean = "mean", 
-              sd = "sd", 
-              min = "min", 
-              # p25 = quantile(probs = 0.25),
-              med = "median",
-              # p75 = quantile(probs = 0.75),
-              max = "max") %>% print()
-}
+row_imc <- df_1 %>% 
+  summarise(n = sum(!is.na(imc)),
+            mean = mean(imc, na.rm = TRUE),
+            sd = sd(imc, na.rm = TRUE), 
+            min = min(imc, na.rm = TRUE), 
+            p25 = quantile(imc, probs = 0.25, na.rm = TRUE),
+            med = median(imc, na.rm = TRUE),
+            p75 = quantile(imc, probs = 0.75, na.rm = TRUE),
+            max = max(imc, na.rm = TRUE)) %>%
+  mutate(var = "IMC (kg/m2)") %>%
+  relocate(var, .before = n)
 
+row_pas <- df_1 %>% 
+  summarise(n = sum(!is.na(pas)),
+            mean = mean(pas, na.rm = TRUE),
+            sd = sd(pas, na.rm = TRUE), 
+            min = min(pas, na.rm = TRUE), 
+            p25 = quantile(pas, probs = 0.25, na.rm = TRUE),
+            med = median(pas, na.rm = TRUE),
+            p75 = quantile(pas, probs = 0.75, na.rm = TRUE),
+            max = max(pas, na.rm = TRUE)) %>%
+  mutate(var = "PAS (mmHg)") %>%
+  relocate(var, .before = n)
+
+## on a vu les fonctions Rbases rbind() et cbind() pour fusionner des vecteurs 
+## ou des matrices ou des data.frame par rang ou par colonnes 
+## Au sein du package dplyr du tidyverse, nous avons également les fonctions 
+## bind_rows() et bind_cols() (avec des options et propriétés un peu différentes)
+bind_rows(row_imc, row_pas)
+
+
+## 5.3) variables qualitatives ----
+### pour décrire les variables qualitatives, on peut utiliser les fonctions
+### table et prop.table() de Rbase
+table(df_1$traitL)
+prop.table(table(df_1$traitL))
+
+### il existe également une fonction fct_count() du package forcats
+### pour dénombrer le nombre de réponses par modalités
+fct_count(df_1$traitL)
+# par rapport à la fonction table, la fonction fct_count()
+# contient 2 arguments intéressants: 
+# sort = TRUE pour trier les résultats du plus fréquent au moins fréquent
+# prop = TRUE pour indiquer les résultats en %
+fct_count(df_1$traitL, 
+          sort = TRUE, # les résultats étaient déjà triés du + au - fréquent
+          prop = TRUE)
+
+
+### combiner dans une seule table la description des variables sex et trait
+bind_rows(tibble(f = meta_df_1$label[meta_df_1$var == "sex"][1]),
+          fct_count(df_1$sexL, sort = TRUE, prop = TRUE),
+          tibble(f = meta_df_1$label[meta_df_1$var == "trait"][1]),
+          fct_count(df_1$traitL, sort = TRUE, prop = TRUE)) %>%
+  rename("Variable" = f, 
+         "%" = p)
+
+
+# 6) Représentations grahpiques ----
+## 6.1) Distributions univariées ----
+## Le tidyverse utilise le package ggplot2 https://ggplot2.tidyverse.org/articles/ggplot2.html 
+## pour réaliser des graphiques
+## Autres aides : 
+?ggplot2
+## voir le livre R for data science pour de nombreux exemples de représentations
+## graphiques
+## https://posit.co/wp-content/uploads/2022/10/data-visualization-1.pdf
+## il existe un livre ggplot2 https://ggplot2-book.org/
+
+## Les figures sont composées de calques successifs
+## on commence par indiquer une première ligne avec comme argument : 
+##  - les données
+##  - les variables à utiliser (aesthetics)
+ggplot(data = df_1, mapping = aes(x = imc))
+## cela prépare un fond de graphique (1er calque)
+
+## puis on ajoute un ou plusieurs calques (avec des signes +)
+## pour les variables quantitatives 
+##  - histogram
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_histogram()
+# pour voir les options possibles de l'histogramme : 
+?geom_histogram
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_histogram(bins = 10, # nombre de "rectangles" default = 30
+                 colour = "black", # couleur du contour
+                 linewidth = 3, # épaisseur de ligne
+                 fill = "dodgerblue3")
+
+##  - densité de kernel
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_density()
+# pour voir les options possibles de les densités de kernel : 
+?geom_density
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_density(bw = "nrd", n = 100, # bandwidth selector
+               kernel = "rectangular") # kernel function
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_density(bw = "nrd0", # default bandwidth selector
+               kernel = "gaussian", # default kernel function
+               colour = "red", # couleur du contour
+               linewidth = 2, # épaisseur de ligne
+               linetype = 4) # type de ligne (dotdash)
+# type de lignes : 
+# 0 = blank, 1 = solid, 2 = dashed, 3 = dotted, 4 = dotdash, 5 = longdash, 
+# 6 = twodash
+
+##  - boxplot
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_boxplot()
+
+# représentation sur l'axe des Y
+ggplot(data = df_1, mapping = aes(y = imc)) +
+  geom_boxplot()
+
+# pour voir les options possibles de les densités de kernel : 
+?geom_boxplot
+ggplot(data = df_1, mapping = aes(x = imc)) +
+  geom_boxplot(outliers = TRUE, # afficher les outliers
+               outlier.colour = "blue",
+               outlier.fill = "green",
+               outlier.shape = 7, # symbol des outlier voir ?shape
+               outlier.size = 5, # taille des outliers
+               outlier.stroke = 0.1, # épaisseur de trait des outliers
+               outlier.alpha = NULL, # transparence des outliers
+               staplewidth = 0.1) # largeur de la moustache
+
+### Diagrammes en barres, appliqués aux "facteurs"
+ggplot(data = df_1, mapping = aes(x = sexL)) +
+  geom_bar()
+# pour voir les options possibles de les diagrammes en barres : 
+?geom_bar
+
+# sur l'axe des Y plutôt que des X
+ggplot(data = df_1, mapping = aes(y = sexL)) + # ici y = sexL
+  geom_bar()
+
+# afficher des % plutôt que des comptes
+## ? je ne sais pas faire !!
+
+# options
+ggplot(data = df_1, mapping = aes(x = sexL)) +
+  geom_bar(mapping = aes(y = after_stat(prop)), # pour des % plutôt que des N
+           just = 0.1, # emplacement : 0=à gauche, 1=à droite, 0.5=centré
+           width = 0.5, # largeur de bande (0.9 par défaut)
+           colour = "red", # couleur de ligne
+           linewidth = 2, # largeur de ligne
+           fill = "orange2") # couleur de remplissage
+
+
+
+
+## 6.2) Distributions bivariées ----
+
+## on peut relabeliser les axes avec un filtre "labs"
+
+
+### boxplots
+### PAS en fonction du sex. 
+ggplot(data = df_1, mapping = aes(x = sexL, y = pas)) +
+  geom_boxplot(outliers = TRUE, staplewidth = 0.2) + 
+  labs(x = "Sexe", y = "PAS (mmHg)", 
+       title = "PAS en fonction du sexe", 
+       subtitle = "sous-titre")
+
+### PAS en fonction du traitement
+ggplot(data = df_1, mapping = aes(x = traitL, y = pas)) +
+  geom_boxplot(outliers = TRUE, staplewidth = 0.2) + 
+  labs(x = "Traitement", y = "PAS (mmHg)", 
+       title = "PAS en fonction du traitement")
+
+### PAS en fonction du traitement et du sexe 
+ggplot(data)
+
+### Répartition du sexe par traitement
+ggplot(data = df_1, mapping = aes(x = traitL, fill = sexL)) +
+  geom_bar(position = "stack") + # empilé
+  labs(x = "Traitement", title = "Sexe et traitement")
+
+ggplot(data = df_1, mapping = aes(x = traitL, fill = sexL)) +
+  geom_bar(position = "dodge") + # côte à côte
+  labs(x = "Traitement", title = "Sexe et traitement")
+
+### On peut représenter un nuage de points de la PAS en fonction de l'IMC : 
+### où les hommes et les femmes ont deux symboles différents et les traitements 
+### deux couleurs différentes
+ggplot(data = df_1, mapping = aes(y = pas, 
+                                  x = imc,
+                                  shape = sexL,
+                                  color = traitL)) +
+  geom_point() 
+
+ggplot(data = df_1, mapping = aes(y = pas, x = imc, shape = sexL, 
+                                  color = traitL)) +
+  geom_point(size = 2) + # taille des symboles
+  labs(x = "IMC (kg/m²)", y = "PAS (mmHg)", # titre et axes
+       title = "PAS en fonction de l'IMC") + 
+  scale_colour_discrete(name = "Traitement", # 1ère légende liée aux couleurs
+                          breaks = c("Placebo", "Traitement A", "Traitement B"),
+                          labels = c("Placebo", "Traitement", "Traitement B")) +
+  scale_shape_discrete(name = "Sex", # 2ème légende liée aux symboles
+                       breaks = c("Féminin", "Masculin"),
+                       labels = c("Féminin", "Masculin")) + 
+  theme(axis.text = element_text(size = 12)) # taille des valeurs sur les axes
+    
+
+# si on veut choisir les symboles ?? et le faire en noir et blanc
+
+# 7) Analyses bivariées ----
+## 7.1) Variable quantitative $\times$ qualitative ----
+## dplyr permet décrire une variable quantitative en fonction d'une variable, 
+## qualitative, avec group_by
+df_1 %>%
+  group_by(sexL)
+# notez qu'en description, le tibble indique qu'il considère la variable sexL
+# comme une variable de groupe => cela pris en compte pour les analyses qui 
+# suivront dans le pipe.
+# Si vous avez sauvegardé une base tibble avec une propriété de sous-groupe, 
+# vous pouvez la supprimer avec la fonction ungroup()
+df_1 %>%
+  group_by(sexL) %>%
+  ungroup()
+
+## IMC en fonction du sexe
+df_1 %>%
+  group_by(sexL) %>%
+  summarise(n = sum(!is.na(imc)), # nombre de non-manquants
+            mean = mean(imc), 
+            sd = sd(imc))
+
+## PAS en fonction du traitement
+df_1 %>%
+  group_by(traitL) %>%
+  summarise(n = sum(!is.na(pas)), # nombre de non-manquants
+            mean = mean(pas), 
+            sd = sd(pas))
+
+## 7.2) Variable qualitative $\times$ qualitative ----
+### Décrire les effectifs et % de la variable sexL (en lignes),
+### en fonction de la variable traitL (en colonnes) 
+df_1 %>%
+  group_by(sexL) %>%
+  summarise("Placebo, n" = sum(trait == 1), # effectif
+            "Placebo, %" = mean(trait == 1) * 100, # % en ligne (selon le sex)
+            "Traitement A, n" = sum(trait == 2), 
+            "Traitement A, %" = mean(trait == 2) * 100,
+            "Traitement B, n" = sum(trait == 3), 
+            "Traitement B, %" = mean(trait == 3) * 100)
+
+## 7.3) Comparer 2 moyennes ----
+# le test de Student se fait avec les commandes R base
+# on peut vérifier la normalité des distributions dans chaque groupe
+# avec ggplot2
+ttest_pas_sex <- df_1 %>% t.test(data = ., # placeholder pour indiquer la base de données
+                                 pas ~ sexL, # formula
+                                 var.equal = TRUE) # pour le test de Student
+
+# test de Levene
+df_1 %>% car::leveneTest(data = ., 
+                         pas ~ sexL) 
+
+# distribution 
+df_1 %>% 
+  filter(sex == 0) %>%
+  ggplot(aes(x = pas)) + 
+  geom_density() + 
+  stat_function(fun = dnorm, 
+                args = list(mean = mean(df_1$pas[df_1$sex == 0]), 
+                            sd = sd(df_1$pas[df_1$sex == 0])),
+                col = "red", lwd = 0.5, lty = "dashed")
+
+df_1 %>% 
+  filter(sex == 1) %>%
+  ggplot(aes(x = pas)) + 
+  geom_density() + 
+  stat_function(fun = dnorm, 
+                args = list(mean = mean(df_1$pas[df_1$sex == 1]), 
+                            sd = sd(df_1$pas[df_1$sex == 1])),
+                col = "red", lwd = 0.5, lty = "dashed")
+
+# on peut également combiner 2 QQ-plots 
+df_1 %>% ggplot(mapping = aes(sample = pas, colour = sexL)) + 
+  stat_qq() + 
+  stat_qq_line() 
+
+# si on veut appliquer un test de Wilcoxon
+df_1 %>% wilcox.test(data = ., # placeholder
+                     pas ~ sex) # p-value = 1.529e-06
+
+
+## 7.4) Comparer 3 moyennes ou plus ----
+# on peut utiliser le pipe pour enchaîner les analyses nécessaires à une anova
+anova_pas_trait <- df_1 %>%
+  lm(pas ~ traitL, 
+     data = .) %>% # placeholder
+  anova()
+# un inconvenient est qu'aucun des objets calculés n'est en mémoire, 
+# on récupérer simplement le résultat de la table d'anova (dernière commande)
+
+# pour tester la normalité avec un QQ-plot sur les résidus du modèle linéaire : 
+mod_pas_trait <- df_1 %>%
+  lm(pas ~ traitL, 
+     data = .)
+
+ggplot(mapping = aes(sample = mod_pas_trait$residuals)) + 
+  geom_qq() + 
+  stat_qq() + stat_qq_line() # ajoute la ligne de référence
+
+## si on veut faire un test de Kruskal Wallis
+df_1 %>% 
+  kruskal.test(pas ~ traitL, data = .) # p-value = 7.336e-05
+
+
+## 7.5) Comparer des pourcentages ----
+# on utilise la même fonction qu'en R base, exemple avec un pipeline
+chi2 <- df_1 %>% 
+  with(table(sexL, traitL)) %>%
+  chisq.test(correct = FALSE)
+
+# effectifs attendus
+chi2$expected
+
+# test exact de Fisher
+df_1 %>% 
+  with(table(sexL, traitL)) %>%
+  fisher.test()
+
+## 7.6) Corrélations ----
+# corrélation de Pearson entre PAS et IMC
+# corrélations de Pearson et de Spearman
+df_1 %>% with(cor.test(imc, pas, method = "pearson"))
+df_1 %>% with(cor.test(imc, pas, method = "spearman"))
+
+
+# 8) Analyse multivariée ----
+# les commandes sont les mêmes, exemple avec pipeline
+model <- df_1 %>% lm(pas ~ traitL + sexL + imc, 
+                     data = .)
+
+df_1 %>% 
+  lm(pas ~ traitL + sexL + imc, 
+     data = .) %>%
+  summary()
+
+# graphique de diagnostic des rédidus en fonction des valeurs prédites
+ggplot() + 
+  geom_point(mapping = aes(y = model$residuals, # résidus
+                           x = model$fitted.values)) + # valeurs prédites
+  geom_smooth(mapping = aes(y = model$residuals, 
+                           x = model$fitted.values)) + 
+  labs(y = "résidus", x = "valeurs prédites")
+
+# par défaut, geom_smooth fait un spline
+# si on veut une fonction loess comme dans l'exemple du chap 2
+ggplot() + 
+  geom_point(mapping = aes(y = model$residuals, # résidus
+                           x = model$fitted.values), # valeurs prédites
+             shape = 1) + # symbole des points
+  geom_smooth(mapping = aes(y = model$residuals, 
+                            x = model$fitted.values),
+              method = "loess", # méthode de lissage
+              colour = "red", # couleur de ligne
+              linetype = 2, # type de ligne
+              linewidth = 0.5, # largeur de ligne
+              se = FALSE) + # supprime la bande d'IC95% 
+  labs(y = "résidus", x = "valeurs prédites")
+               
+## QQ plot des résidus
+ggplot(mapping = aes(sample = model$residuals)) + 
+  geom_qq(shape = 1) + 
+  stat_qq(shape = 1) + stat_qq_line() 
